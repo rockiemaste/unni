8c8fd5d98753f3d419a0f0a8afd309322cfd37c6
rockiemaste <93933726+rockiemaste@users.noreply.github.com>
2021-11-11 11:30:32 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/@Unni0240start=help"),
+            InlineKeyboardButton('📢 Updates', url='https://t.me/StrangersMusicchat')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
